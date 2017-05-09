3e38b9daca9ba5a25f3bcf17453f4c177c7921ac
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-05-09 18:32:28 +0200

    verder uitgewerkt. problemen met npm

--- nonRoot.sh
+++ nonRoot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "als vagrantuser uitgevoerd"
-cd /var/www/dops-lab2-skidekeersmaecker
+cd /var/www/dops_lab2
 nodejs server.js  > /home/ubuntu/stdout.txt 2> /home/ubuntu/stderr.txt &
--- provision_node.sh
+++ provision_node.sh
@@ -1,14 +1,15 @@
 apt-get update
 apt-get install nodejs
+apt-get install npm
 
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
 
 mkdir /var/www
 cd /var/www
-if [ ! -d /var/www/dops-lab2-skidekeersmaecker ]; then
-	git clone https://github.com/AP-Elektronica-ICT/dops-lab2-skidekeersmaecker.git
+if [ ! -d /var/www/dops_lab2 ]; then
+	git clone https://github.com/skidekeersmaecker/dops_lab2.git
 fi
-cd dops-lab2-skidekeersmaecker
+cd dops_lab2
 git pull origin master
 npm install
 runuser -l ubuntu -c '/./vagrant/nonRoot.sh'
