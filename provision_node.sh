62363aeabb5d4b86f17fa988d2c991a8b61ed212
Ski De Keersmaecker <s.dekeersmaecker@gmail.com>
2017-05-11 09:00:15 +0200

    changed port

--- provision_node.sh
+++ provision_node.sh
@@ -1,8 +1,11 @@
 apt-get update
-apt-get install nodejs
-apt-get install npm
+apt-get install -y npm
+apt-get install -y nodejs
+apt-get install -y build-essential
 
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+iptables -t nat -A PREROUTING -p tcp --d 127.0.0.1 -j REDIRECT --to-port 3000
+
 
 mkdir /var/www
 cd /var/www
@@ -11,5 +14,10 @@ if [ ! -d /var/www/dops_lab2 ]; then
 fi
 cd dops_lab2
 git pull origin master
+
+npm init
+npm install express --save
+npm install body-parser --save
 npm install
+
 runuser -l ubuntu -c '/./vagrant/nonRoot.sh'
--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var app = express();
 
 app.use(express.static('client'));
 
-app.listen(80);
+app.listen(3000);
